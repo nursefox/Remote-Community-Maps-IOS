665b14629dd644eacd4a3c838bdca7fcb178ebcf
Benjamin Fox <benjamin@nursefox.com>
2024-07-14 10:15:45 +0930

    New locations and front and sorting enabled

---
+++ Remote Community Maps/Views/RemoteCommunitiesCountryView.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteCommunitiesCountryView.swift
+//  Remote Community Maps
+//
+//  Created by Benjamin Fox on 14/7/2024.
+//
+
+import SwiftUI
+
+struct RemoteCommunitiesCountryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemoteCommunitiesCountryView()
+}
